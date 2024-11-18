b7c9e3b88979af2d2108490a095fe5e69d868354
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-18 15:39:46 +0100

    10 emails from boolean api using http request with axios api

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,5 +3,18 @@ Attraverso l'apposita API di Boolean
 https://flynn.boolean.careers/exercises/api/random/mail
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
+//* DOM elements selection
+const $result = document.getElementById("result");
+// =============================================================================
+// **************** AXIOS API **************************
+// =============================================================================
+for (let i = 0; i < 10; i++) {
+    axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => response.data)
+        .then((data) => {
+            $result.insertAdjacentHTML("beforeend", `<span>${data.response}</span><br>`);
+        })
+        .catch((error) => console.error(error));
+}
 
-console.log(document.querySelector("h1").innerHTML);
