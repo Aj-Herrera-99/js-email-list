29e9b6eacdc6d2a845726a4e2bd45ef1dac75cbc
Aj-Herrera-99 <ajherrera999@gmail.com>
2024-11-18 16:40:30 +0100

    new button and functions for http requests with fetch API

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,27 +5,56 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
 //* DOM elements selection
 const $result = document.getElementById("result");
-const $button = document.querySelector("button");
+const $buttonAxios = document.getElementById("btn-axios");
+const $buttonFetch = document.getElementById("btn-fetch");
 // =============================================================================
 // **************** AXIOS API **************************
 // =============================================================================
 //* EVENT LISTENERS
-$button.addEventListener("click", (e) => handleClick(e, $result));
+$buttonAxios.addEventListener("click", (e) => handleClickAxios(e, $result));
+$buttonFetch.addEventListener("click", (e) => handleClickFetch(e, $result));
 //* EVENT HANDLERS
-function handleClick(e, $element) {
+function handleClickAxios(e, $element) {
     $element.innerHTML = "";
     for (let i = 0; i < 10; i++) {
-        generateRandomMailFrom(
+        axiosGenerateRandomMailFrom(
             "https://flynn.boolean.careers/exercises/api/random/mail",
             $element
         );
     }
 }
+
+function handleClickFetch(e, $element) {
+    $element.innerHTML = "";
+    for (let i = 0; i < 10; i++) {
+        fetchGenerateRandomMailFrom(
+            "https://flynn.boolean.careers/exercises/api/random/mail",
+            $element
+        );
+    }
+}
+
+// =============================================================================
+// **************** FETCH API **************************
+// =============================================================================
+
+
 //! FUNCTIONS
-function generateRandomMailFrom(url, $element) {
+function axiosGenerateRandomMailFrom(url, $element) {
     axios
         .get(url)
         .then((response) => response.data)
+        .then((data) => {
+            $element.insertAdjacentHTML(
+                "beforeend",
+                `<span class="d-inline-block px-2 py-1 text-bg-info rounded-2 my-1">${data.response}</span><br>`
+            );
+        })
+        .catch((error) => console.error(error));
+}
+function fetchGenerateRandomMailFrom(url, $element) {
+    fetch(url)
+        .then((response) => response.json())
         .then((data) => {
             $element.insertAdjacentHTML(
                 "beforeend",
